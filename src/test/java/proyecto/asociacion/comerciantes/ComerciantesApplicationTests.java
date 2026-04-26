66c0ab254b164017fccfe868d310b9fbd84fc9db
CAlfred1 <datatotalftp1@gmail.com>
2026-04-26 18:04:13 -0500

    Configuración inicial del backend Spring Boot

---
+++ src/test/java/proyecto/asociacion/comerciantes/ComerciantesApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.asociacion.comerciantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComerciantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
